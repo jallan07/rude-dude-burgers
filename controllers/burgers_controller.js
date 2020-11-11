2426f9e552ebd200d4cb3ed81870fd964661feae
Josh Allan <allan.josh07@gmail.com>
2020-11-11 15:44:51 -0500

    Created the controller folder and built out the foundation for the burgers_controller.js file. Will come back to fully flesh out later. Have not yet added routes to the controller, but the dependencies and exports are declared.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,12 @@
+// dependencies
+const express = require("express");
+// Import the model (burger.js) to use its database functions.
+const burger = require("../models/burger.js");
+
+// create the router
+const router = express.Router();
+
+// Create all our routes and set up logic within those routes where required.
+
+// export the router
+module.exports = router;
