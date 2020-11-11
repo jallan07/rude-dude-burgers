d2e49dedc1d3d2db52e884c50f8a35143ea30e12
Josh Allan <allan.josh07@gmail.com>
2020-11-11 15:12:53 -0500

    Added some basic functionality to the selectAll and insertOne methods within the ORM file. Will come back later to fully flesh out, but they currently do select and insert properly.

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,29 @@
 // dependencies
+const { query } = require("express");
 const connection = require("./connection");
 
 // ORM methods for controllers
 const orm = {
-	selectAll: function () {},
-	insertOne: function () {},
+	selectAll: function (table) {
+		connection.query("SELECT * FROM ??", [table], function (err, data) {
+			if (err) throw err;
+			console.table(data);
+		});
+	},
+	insertOne: function (table, col1, col2, burger_name, devoured_status) {
+		connection.query(
+			"INSERT INTO ?? (??, ??) VALUES (?, ?)",
+			[table, col1, col2, burger_name, devoured_status],
+			function (err, data) {
+				if (err) throw err;
+				// log rows that have been affected
+				let affected = data.affectedRows;
+				console.log({ affected });
+				// log in table format the full table of burgers in our database, including the one that was just added
+				console.log(orm.selectAll("burgers"));
+			}
+		);
+	},
 	updateOne: function () {},
 };
 
