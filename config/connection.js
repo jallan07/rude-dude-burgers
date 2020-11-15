d2c11602a1a5854b1b60e38a7b31eb6ca9fddfc5
Josh Allan <allan.josh07@gmail.com>
2020-11-15 14:21:53 -0500

    Updated the database connection so that JAWS db is integrated (for heroku deployment purposes).

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // dependencies
 const mysql = require("mysql");
 
-// create the conneciton variable
-const connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	user: "root",
-	password: "mypasswordis6Str!pes",
-	database: "burgers_db",
-});
+// create the connection variable
+var connection;
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		user: "root",
+		password: "mypasswordis6Str!pes",
+		database: "burgers_db",
+	});
+}
 
 // establish a connection to the database
 connection.connect(function (err) {
